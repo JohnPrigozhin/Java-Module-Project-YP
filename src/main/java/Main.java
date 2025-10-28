dd3ea8e669dae2f1887fad0224525cfc9b836c4c
JohnPrigozhin <krown62@gmail.com>
2025-10-28 17:33:26 +0300

    Созданы классы Car & Race

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for (int i = 1; i <= 3; i++) {
+
+            String name = "";
+            while (name.trim().isEmpty()) {
+                System.out.println("Введите название машины №" + i + ":");
+                name = scanner.nextLine();
+                if (name.trim().isEmpty()) {
+                    System.out.println("Название не может быть пустым!");
+                }
+            }
+
+
+            int speed = 0;
+            boolean valid = false;
+
+            while (!valid) {
+                System.out.println("Введите скорость машины №" + i + ":");
+
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (speed > 0 && speed <= 250) {
+                        valid = true;
+                    } else {
+                        System.out.println("Неправильная скорость! Должна быть от 1 до 250.");
+                    }
+                } else {
+                    System.out.println("Ошибка: введите целое число!");
+                    scanner.nextLine();                }
+            }
+
+            Car car = new Car(name.trim(), speed);
+            race.checkLeader(car);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.getWinner());
+        scanner.close();
     }
 }
